5edf6c0d4073e16b519ae974b4df1042a6deceea
S-Jo <sneha21j@gmail.com>
2020-05-22 17:52:06 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,19 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Universe!"
+    return "Hello World!"
 
 @app.route("/webhook")
 def webhook():
-    #return "Hello from Webhook!"
-    #req = request.get_json(silent=True, force=True)
-    #try:
-    #    action = req.get('queryResult').get('action')
-    #except AttributeError:
-    #    return 'json error'
-    # return a fulfillment response
-    return jsonify("sneha")
-    #{'fulfillmentText': 'This is a response from webhook.'}
+    return jsonify("Webhook Successfull")
 
 if __name__ == "__main__":
     app.run()
