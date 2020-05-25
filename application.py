7c51db3f307ba85a993eee441e678b8e94f40246
S-Jo <sneha21j@gmail.com>
2020-05-25 20:50:26 +0530

    Update application.py

--- application.py
+++ application.py
@@ -12,27 +12,17 @@ def results():
     try:
         action = req.get("queryResult").get("action")
     except AttributeError:
-        return {"JSON Request is not sent to the server"}
+        return {{"fulfillmentText": "JSON Request is not sent to the server"}
     
-    return {"fulfillmentText": "This is a response from webhook."}
-
-    '''# build a request object
-    req = request.get_json(force=True)
-
-    # fetch action from json
-    try:
-        action = req.get("queryResult").get("action")
-    except AttributeError:
-        return "JSON Request is not sent to the server"
-
     if action == "input.unknown":
-        return "Received Fallback intent"
+        return {"fulfillmentText": "Received Fallback intent"}
     elif: 
         action == "input.welcome"
-        return "Received Welcome intent"
+        return {"fulfillmentText": "Received Welcome intent"}
     else:
-        return "Intent not recognized"
-        '''
+        return {"fulfillmentText": "Intent not recognized"}
+    
+    #return {"fulfillmentText": "This is a response from webhook."}
     
 @app.route("/Webhook", methods=['GET', 'POST'])
 def webhook():
