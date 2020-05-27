7d724cf606d36dffb7dc38942d12d3203e065605
S-Jo <sneha21j@gmail.com>
2020-05-27 11:54:03 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,23 +6,6 @@ def hello():
     return "Hello World!"
 
 def results():
-    # build a request object
-    try:
-        req = request.get_json(force=True)
-    except AttributeError:
-        return "No JSON Request is received. Try running the API from Dialogflow"
-    
-    # fetch action from json
-    try:
-        action = req.get("queryResult").get("action")
-    except AttributeError:
-        return "No JSON Request is received. Try running the API from Dialogflow"
-   ''' 
-    if action == "Product":
-        product = req.get("queryResult").get("parameters").get("Product")
-        filterr = req.get("queryResult").get("parameters").get("Filter")
-        return filterr        
-    '''   
     return {"fulfillmentText": "This is a response from webhook."}
     
 @app.route("/webhook", methods=['GET', 'POST'])
