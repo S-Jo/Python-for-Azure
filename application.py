84c4a4e00dd5d21256daa28974209ff8bf09022c
S-Jo <sneha21j@gmail.com>
2020-05-27 11:49:48 +0530

    Update application.py

--- application.py
+++ application.py
@@ -17,15 +17,13 @@ def results():
         action = req.get("queryResult").get("action")
     except AttributeError:
         return "No JSON Request is received. Try running the API from Dialogflow"
-    
+   ''' 
     if action == "Product":
         product = req.get("queryResult").get("parameters").get("Product")
         filterr = req.get("queryResult").get("parameters").get("Filter")
         return filterr        
     '''   
-        return {"fulfillmentText:" + product + " - " + filterr}
-    #return {"fulfillmentText": "This is a response from webhook."}
-    '''
+    return {"fulfillmentText": "This is a response from webhook."}
     
 @app.route("/webhook", methods=['GET', 'POST'])
 def webhook():
