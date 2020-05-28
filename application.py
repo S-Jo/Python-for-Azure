128de80ba4c2ac49e74865767ae9289ca0a52cb3
S-Jo <sneha21j@gmail.com>
2020-05-28 15:09:34 +0530

    Update application.py

--- application.py
+++ application.py
@@ -7,22 +7,54 @@ app = Flask(__name__)
 def hello():
     return "App is working"
 
-def results():
+def results(filterr,fundname):
+    filtertype = filterr
+    fund = fundname
     server = 'tcp:srvforpoc.database.windows.net,1433'
     database = 'DBBotServiceData'
     username = 'srvforpoc'
     password = 'Server@123'
-    driver= '{ODBC Driver 17 for SQL Server}'
-    cnxn = pyodbc.connect(('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password),timeout=5)
+    driver= '{ODBC Driver 13 for SQL Server}'
+    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = cnxn.cursor()
     ans = cursor.execute("SELECT * FROM [dbo].[bot_service] where Fund = 'franklin asian equity fund'")
     row = cursor.fetchone()
-    fund = "gyugubi"
-    return "AUM for the " + fund + " is " + str(row[1]) + " Crore."
+    if filtertype == "AUM":
+        return "AUM for the " + fund + " is " + str(row[1]) + " Crore."
+    elif filtertype == "Expense Ratio":
+        return "Expense Ratio for the " + fund + " is " + str(row[2]) + "."
+    elif filtertype == "Fund Manager":
+        return "Fund Manager for the " + fund + " is " + row[3] + "."
+    elif filtertype == "Details":
+        return fund + " has AUM of " + str(row[1]) + " Crore, Expense Ratio, " + str(row[2]) + " and managed by " + row[3] +"."
+    else:
+         return "Something went wrong"
+
+# function for response
+def fetchjson():
+    '''req = request.get_json(force=True)
+    action = req.get('queryResult').get('action')
+    
+    if action == "FundAction":
+        fund = req.get("queryResult").get("parameters").get("Fund")
+        filterr = req.get("queryResult").get("parameters").get("filter")
+        res = results(filterr,fund)
+        return res
+        #return results(filterr,fund)
+    else:
+        return "Intent not recognized"
+        '''
+    
+    #To test local
+    p1 = "Expense Ratio"
+    p2 = "franklin asian equity fund"
+    return results(p1,p2)
     
 @app.route("/webhook", methods=['GET', 'POST'])
 def webhook():
-    return make_response(jsonify({'fulfillmentText': results()}))
+    #return make_response(jsonify(fetchjson()))
+    #res = fetchjson()
+    return make_response(jsonify({'fulfillmentText': fetchjson()}))
 
 if __name__ == "__main__":
     app.run()
