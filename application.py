b3f9b89cd689ab924e76b8edb7d472285f6226a9
S-Jo <sneha21j@gmail.com>
2020-06-08 13:47:54 +0530

    Update application.py

--- application.py
+++ application.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def hello():
     return "App is working"
 
-def results(filterr,fundname):
+def results(action,filterr,fundname):
+    action == action
     filtertype = filterr
     fund = fundname
     server = 'tcp:srvforpoc.database.windows.net,1433'
@@ -22,16 +23,28 @@ def results(filterr,fundname):
     cursor = cnxn.cursor()
     ans = cursor.execute("SELECT * FROM [dbo].[bot_service] where Fund = '" + fund + "'")
     row = cursor.fetchone()
-    if "AUM" in filtertype:
-        return "AUM for the " + fund + " is " + str(row[1]) + " Crore."
-    elif "Expense Ratio" in filtertype:
-        return "Expense Ratio for the " + fund + " is " + str(row[2]) + "."
-    elif "Fund Manager" in filtertype:
-        return "Fund Manager for the " + fund + " is " + row[3] + "."
-    elif "Details" in filtertype:
-        return fund + " has AUM of " + str(row[1]) + " Crore, Expense Ratio is " + str(row[2]) + " and managed by " + row[3] +"."
+    if action == "singlefilter":
+        if "AUM" in filtertype:
+            return "AUM for the " + fund + " is " + str(row[1]) + " Crore."
+        elif "Expense Ratio" in filtertype:
+            return "Expense Ratio for the " + fund + " is " + str(row[2]) + "."
+        elif "Fund Manager" in filtertype:
+            return "Fund Manager for the " + fund + " is " + row[3] + "."
+        elif "Details" in filtertype:
+            return fund + " has AUM of " + str(row[1]) + " Crore, Expense Ratio is " + str(row[2]) + " and it's managed by " + row[3] +"."
+        else:
+            return "Something went wrong in action singlefilter"
+    elif action == "dualfilter":
+        if "AUM and Expense Ratio" in filtertype:
+            return fund + " has AUM of " + str(row[1]) + " Crore, Expense Ratio is " + str(row[2]) +"."
+        if "AUM and Fund Manager" in filtertype:
+            return fund + " has AUM of " + str(row[1]) +  " and it's managed by " + row[3] +"."
+        if "Fund Manager and Expense Ratio" in filtertype: 
+            return " Expense Ratio of " + fund + " is " + str(row[2]) + " and it's managed by " + row[3] +"."
+        else:
+            return "Something went wrong in action dualfilter"
     else:
-         return "Something went wrong"
+        return "Intent/Action not recognized"
 
 # function for response
 def fetchjson():
@@ -44,7 +57,14 @@ def fetchjson():
         fund = fund.replace("']","")             
         filterr = str(req.get("queryResult").get("parameters").get("Filter"))
         #return filterr
-        return results(filterr,fund)
+        return results(action,filterr,fund)
+    elif action == "dualfilter":
+        p1 = str(req.get("queryResult").get("parameters").get("Fund"))
+        fund = p1.replace("['","")
+        fund = fund.replace("']","")             
+        filterr = str(req.get("queryResult").get("parameters").get("FilterDual"))
+        #return filterr
+        return results(action,filterr,fund)
     elif action == "input.welcome":
         return "Hey! Welcome to Franklin Templeton Voice Assistance. Please ask your question"
     else:
@@ -65,4 +85,3 @@ def webhook():
 
 if __name__ == "__main__":
     app.run()
-    
