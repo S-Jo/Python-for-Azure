d4010fc6fcb8cabaeb803839c9ecba815c202b2f
S-Jo <sneha21j@gmail.com>
2020-06-11 17:38:53 +0530

    Update application.py

--- application.py
+++ application.py
@@ -18,40 +18,48 @@ def results(action,filterr,fundname,fundmanager):
     username = 'srvforpoc'
     password = 'Server@123'
     driver= '{ODBC Driver 17 for SQL Server}'
+
     try:
         cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     except pyodbc.Error as err:
         return "Couldn't connect to database"
+    
+    #Single Filter & Dual Filter Intent
     cursor1 = cnxn.cursor()
     query1 = cursor1.execute("SELECT * FROM [dbo].[bot_service] where Fund = '" + fund + "'")
-    row = cursor1.fetchone()
+    ans1 = cursor1.fetchone()
+
+    #Funds Under Management    
     cursor2 = cnxn.cursor()
     query2 = cursor2.execute("SELECT Fund FROM [dbo].[bot_service] where [Fund Manager] = '" + mgr + "'")
     ans2 = cursor2.fetchall()
     res = []
     for r in ans2:
         res.append(r[0])
-    
+
+    #Function Logic   
     if action == "singlefilter":
         if "aum" in filtertype.lower():
-            return "AUM for the " + fund + " is " + str(row[1]) + " Crore."
+            return "AUM for the " + fund + " is " + str(ans1[1]) + " Crore."
         elif "expense ratio" in filtertype.lower():
-            return "Expense Ratio for the " + fund + " is " + str(row[2]) + "."
+            return "Expense Ratio for the " + fund + " is " + str(ans1[2]) + "."
         elif "fund manager" in filtertype.lower():
-            return "Fund Manager for the " + fund + " is " + row[3] + "."
+            return "Fund Manager for the " + fund + " is " + ans1[3] + "."
         elif "details" in filtertype.lower():
-            return fund + " has AUM of " + str(row[1]) + " Crore, Expense Ratio is " + str(row[2]) + " and it's managed by " + row[3] +"."
+            return fund + " has AUM of " + str(ans1[1]) + " Crore, Expense Ratio is " + str(ans1[2]) + " and it's managed by " + ans1[3] +"."
         else:
-            return "Something went wrong in action singlefilter"
+            return "Please repeat your question"
+
     elif action == "dualfilter":
         if "aum and expense ratio" in filtertype.lower():
-            return fund + " has AUM of " + str(row[1]) + " Crore, Expense Ratio is " + str(row[2])
+            return fund + " has AUM of " + str(ans1[1]) + " Crore, Expense Ratio is " + str(ans1[2])
         if "aum and fund manager" in filtertype.lower():
-            return fund + " has AUM of " + str(row[1]) +  " and it's managed by " + row[3] +"."
+            return fund + " has AUM of " + str(ans1[1]) +  " and it's managed by " + ans1[3] +"."
         if "fund manager and expense ratio" in filtertype.lower(): 
-            return "Expense Ratio of " + fund + " is " + str(row[2]) + " and it's managed by " + row[3] +"."
+            return "Expense Ratio of " + fund + " is " + str(ans1[2]) + " and it's managed by " + ans1[3] +"."
         else:
-            return "Something went wrong in action dualfilter"
+            return "Please repeat your question"
+
     elif action == "Funds&Manager":
         if len(res) == 1:
             return mgr + " manages only one fund and that is " + str(res)
@@ -59,33 +67,40 @@ def results(action,filterr,fundname,fundmanager):
             return mgr + " manages " + str(len(res)) + " funds and they are " + str(res)
         else:
             return "Please repeat your question"
-    else:
-        return "Intent/Action not recognized"
 
-# function for response
+    else:
+        return "I need more training to answer it. Kindly help me by repeating your question."
+
+#This method handles the http requests for the Dialogflow webhook
+#and meant to be used in conjunction with the weather Dialogflow agent
 def fetchjson():
     req = request.get_json(force=True)
-    action = req.get('queryResult').get('action')
-    
+    try:
+        action = req.get('queryResult').get('action')
+    except AttributeError:
+        return 'json error'
+
+    fundname = str(req.get("queryResult").get("parameters").get("Fund"))
+    fund = fundname.replace("['","")
+    fund = fund.replace("']","")  
+
     if action == "singlefilter":
-        p1 = str(req.get("queryResult").get("parameters").get("Fund"))
-        fund = p1.replace("['","")
-        fund = fund.replace("']","")             
         filterr = str(req.get("queryResult").get("parameters").get("Filter"))
         #return filterr
         return results(action,filterr,fund,"")
-    elif action == "dualfilter":
-        p1 = str(req.get("queryResult").get("parameters").get("Fund"))
-        fund = p1.replace("['","")
-        fund = fund.replace("']","")             
+
+    elif action == "dualfilter":            
         filterr = str(req.get("queryResult").get("parameters").get("FilterDual"))
         #return filterr
         return results(action,filterr,fund,"")
+
     elif action == "Funds&Manager":
         mgr = str(req.get("queryResult").get("parameters").get("FundManager"))
         return results(action,'','',mgr)
+
     elif action == "input.welcome":
         return "Greetings from Franklin Voice. How may I assit you?"
+
     else:
         return "Intent not recognized"
     
